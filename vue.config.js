b40875c9411b46b45c9ad723d9ccc779fdb14a21
acestar121 <ace.star.dev@outlook.com>
2022-09-30 16:00:18 +0100

    Adding public path for project

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 module.exports = {
+    publicPath: '/star-wars-table/',
     chainWebpack: config => {
         config.resolve.alias
             .set('@', path.resolve(__dirname, './src'))
